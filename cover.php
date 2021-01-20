13045af3b575b1fe60c70183f32099d93d97173e
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2021-01-20 02:26:11 +0100

    Version 0.0.1

---
+++ cover.php
@@ -0,0 +1,9 @@
+                <section>
+                    <h2></h2>
+                    <p>
+                        <img src="image/GhostBSD_logo.png"><br/>
+                        <img src="image/BSD_wordmark.svg"><br/>
+                        Présentation
+                    </p>
+                </section>
+
---
+++ zeend.php
@@ -0,0 +1,8 @@
+                <section style="text-align: left;">
+                    <h1>The end</h1>
+                    <p>
+                        - <a href="https://slides.com">Try the online editor</a> <br>
+                        - <a href="https://github.com/hakimel/reveal.js">Source code &amp; documentation</a>
+                    </p>
+                </section>
+
