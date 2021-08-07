76827556d1b26701fd6175b3aa762352fdea3db3
Sam <contact@hacktech.dev>
2021-08-07 14:42:27 +0200

    [Update] Several pages

--- lien2.php
+++ lien2.php
@@ -2,7 +2,7 @@
     <h3>Liens</h3>
     <p>
 <li><a href="https://github.com/HackTechDev/prezGhostBSD">https://github.com/HackTechDev/prezGhostBSD</a></li>
-<li><a href="https://projet.hacktech.dev/prez/prezFreeBSD">https://projet.hacktech.dev/prez/prezFreeBSD</a></li>
+<li><a href="https://projet.hacktech.dev/prez/prezFreeBSD">https://projet.hacktech.dev/prezFreeBSD</a></li>
     </p>
 </section>
 
